75b7381e46fd8eb9ea677e6b738ebce1ef0935aa
Saakshi Sharma <saakshi7466@gmail.com>
2026-02-13 22:03:00 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,16 @@
+const mode=document.getElementById("mode");
+const body=document.body;
+let modee="dark";
+mode.addEventListener("click",()=>{
+ if(modee === "dark"){
+     body.classList.add("light");
+     mode.innerHTML="<i class='fa-solid fa-moon'></i>"
+     modee="light";
+ }
+ else{
+    body.classList.remove("light");
+    mode.innerHTML="<i class='fa-solid fa-sun'></i>"
+    modee ="dark";
+ }
+})
+
